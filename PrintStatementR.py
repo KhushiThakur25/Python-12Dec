e5b7120d7ca05cca0fbd5f4b0b596e17de2563da
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-14 13:00:36 +0530

    Add files via upload

---
+++ PrintStatementR.py
@@ -0,0 +1,28 @@
+#this syntax is for single line data
+print("lksjfsd lkjshdsj lksdjhf")
+'''this code is for multi-line
+statement'''
+print("""khushi ksjhdgfsgufucd dfjknhduf
+    lksjdhsd kldjfhiudr
+ujshdusbcfd
+    dfddfbg
+fgbgfbhghngh
+    fgbhgnjgh""")
+'''x = 2.1
+y = 4
+z = x+y
+print("Sum is",x,y,z)
+print("Sum is z",z,sep = " => ",end = "\n")
+print("Sum of x and y is",z)
+print("Sum of",x,"and",y,"is",z)
+print("Sum is %d"%z)
+print("Sum of %f and %d is %f"%(x,y,z))
+print("Sum is {}".format(z))
+print("Sum of {0} and {2} is {1}".format(x,z,y))'''
+#f-string
+x = int(input("Enter the x"))
+y = int(input("Enter the y"))
+z = x+y
+print(f"Sum of {x} and {y} is {z}")
+
+
