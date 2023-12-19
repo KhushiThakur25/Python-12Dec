fa13973df117d8cd0d3b3fa8fbc3339c71210b1d
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-19 13:06:30 +0530

    Add files via upload

---
+++ loop12R.py
@@ -0,0 +1,18 @@
+num = 2
+'''for i in range(5,1,-1):
+    print(f"{num} X {i} = {num*i}")'''
+
+'''while num < 10:
+    print(num)
+    num += 1'''
+'''while True:
+    print(num)
+    num += 1
+    if num == 10:
+        break'''
+flag = True
+while flag:
+    print(num)
+    num += 1
+    if num == 10:
+        flag = False
