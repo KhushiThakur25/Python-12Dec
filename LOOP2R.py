3dc542af2a37d0fddee5a9d552df0ef4b7c8cb68
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-20 13:04:27 +0530

    Add files via upload

---
+++ LOOP2R.py
@@ -0,0 +1,85 @@
+'''for i in range(5):
+    print("Hello world..")'''
+'''x = 0
+while x<5:
+    print(x)
+    x += 2'''
+    
+'''while True:
+    print(x)
+    x += 1
+    if x == 8:
+        break'''
+'''flag = True
+while flag:
+    print(x)
+    x += 1
+    if x == 8:
+        flag = False'''
+'''num = int(input("Enter the number.."))
+if num%2 == 0:
+    print("Number is even..")
+else:
+    print("Number is odd..")'''
+'''a = 55
+b = 9
+if b>a:
+    print("b is greater..")
+else:
+    print("a is greater..")'''
+
+#expression of if-else
+'''num = int(input("Enter the number.."))
+r = "Number is Even" if num%2 == 0 else "Number is Odd"
+print(r)'''
+
+'''num = int(input("Enter the number.."))
+print("Number is Even" if num%2 == 0 else "Number is Odd")'''
+
+#If-else ladder
+x,y,z,a = 10,21,11,65
+if x>y and x>z and x>a:
+    print("X is greatest..")
+elif y>x and y>z and y>a:
+    print("Y is greatest..")
+elif a>x and a>z and a>y:
+    print("A is greatest..")
+else:
+    print("Z is greatest..")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
